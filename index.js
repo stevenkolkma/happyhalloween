e207ce7be2e203fc9c886b795a6c48cb2d6d9ef1
stevenkolkma <114022411+stevenkolkma@users.noreply.github.com>
2025-10-31 23:20:45 +0100

    format

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const helloWorld = "happy halloween!";
-const spookyMessage = "Boo!";
+const helloWorld = "happy halloween! ";
+const spookyMessage = "Boo! ";
 const message = "happy coding!"
+
 console.log(helloWorld, "Have a spooky day!", spookyMessage, message );
